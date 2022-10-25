f39629916a789d9db12a118b56f0e8577ddfb9d3
Adrian <adrian.talonia11@gmail.com>
2022-10-24 23:59:14 -0500

    test

--- src/main/kotlin/com/kotlinapi/model/entity/BaseEntity.kt
+++ src/main/kotlin/com/kotlinapi/model/entity/BaseEntity.kt
@@ -2,12 +2,14 @@ package com.kotlinapi.model.entity
 
 import java.util.*
 import javax.persistence.Column
+import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.MappedSuperclass
 
 @MappedSuperclass
 open class BaseEntity {
     @Id
-    @Column(name = "id", length = 16, unique = true, nullable = false)
-    open val id: UUID = UUID.randomUUID()
+    @GeneratedValue
+    //@Column(name = "id", length = 16, unique = true, nullable = false)
+    val id: UUID = UUID.randomUUID()
 }
