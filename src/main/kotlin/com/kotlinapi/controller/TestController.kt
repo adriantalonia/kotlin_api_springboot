b371399d622154dac9505188520656a23897b214
Adrian <adrian.talonia11@gmail.com>
2022-11-04 23:57:25 -0600

    add controller

--- src/main/kotlin/com/kotlinapi/controller/TestController.kt
+++ src/main/kotlin/com/kotlinapi/controller/TestController.kt
@@ -1,6 +1,5 @@
 package com.kotlinapi.controller
 
-import com.kotlinapi.model.entity.User
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
